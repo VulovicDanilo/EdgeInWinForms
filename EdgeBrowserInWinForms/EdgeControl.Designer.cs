7483fc5787d40ef5223e33fcfc990f93bb093a7f
Danilo Vulovic <danilo.96.vulovic@gmail.com>
2020-03-05 12:36:32 +0100

    init commit

---
+++ EdgeBrowserInWinForms/EdgeControl.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace EdgeBrowserInWinForms
+{
+    partial class EdgeControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLink = new System.Windows.Forms.Button();
+            this.tbxLink = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLink
+            // 
+            this.btnLink.Location = new System.Drawing.Point(1129, 62);
+            this.btnLink.Name = "btnLink";
+            this.btnLink.Size = new System.Drawing.Size(75, 23);
+            this.btnLink.TabIndex = 1;
+            this.btnLink.Text = "Open Page";
+            this.btnLink.UseVisualStyleBackColor = true;
+            this.btnLink.Click += new System.EventHandler(this.btnLink_Click);
+            // 
+            // tbxLink
+            // 
+            this.tbxLink.Location = new System.Drawing.Point(1027, 36);
+            this.tbxLink.Name = "tbxLink";
+            this.tbxLink.Size = new System.Drawing.Size(276, 20);
+            this.tbxLink.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1210, 535);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "What\'s my browser";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1210, 496);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Youtube";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // EdgeControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1328, 580);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbxLink);
+            this.Controls.Add(this.btnLink);
+            this.Name = "EdgeControl";
+            this.Text = "Mini browser with Edge engine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLink;
+        private System.Windows.Forms.TextBox tbxLink;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ EdgeBrowserInWinForms/EdgeControl.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Toolkit.Forms.UI.Controls;
+
+namespace EdgeBrowserInWinForms
+{
+    public partial class EdgeControl : Form
+    {
+        [Obsolete]
+        private WebView web;
+        public EdgeControl()
+        {
+            InitializeComponent();
+            web = new WebView();
+
+            this.Controls.Add(web);
+
+            InitWebView();
+        }
+
+        private void InitWebView()
+        {
+            web.Size = new Size((int)(this.Width * 0.75), this.Height);
+            web.Source = new Uri("https://www.google.rs");
+        }
+
+        private void btnLink_Click(object sender, EventArgs e)
+        {
+            string link = tbxLink.Text;
+
+            tbxLink.Clear();
+
+            if (!link.Contains("https://"))
+            {
+                link = "https://" + link;
+            }
+            web.Source = new Uri(link);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string link = "https://www.whatsmybrowser.org/";
+
+            web.Source = new Uri(link);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string link = "https://www.youtube.com/";
+
+            web.Source = new Uri(link);
+        }
+    }
+}
